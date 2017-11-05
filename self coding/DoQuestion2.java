86b66af1190a5e0fa5dcbf7384ba1adc915a42b5
nnchen <32369145+nnchen@users.noreply.github.com>
2017-11-05 15:02:49 -0800

    Add files via upload

---
+++ self coding/DoQuestion2.java
@@ -0,0 +1,16 @@
+
+public class DoQuestion2 {
+
+	public static void main(String[] args) {
+		int counter = 0;
+		
+		do{
+		
+		System.out.println(counter);
+		counter += 2;
+			
+		} while(counter <=8);
+
+	}
+
+}
