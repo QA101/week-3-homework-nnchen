e7324c7b2fc1adae1c756f11d78880e8003820fe
nnchen <32369145+nnchen@users.noreply.github.com>
2017-11-05 14:26:52 -0800

    Add files via upload

---
+++ self coding/Question5.java
@@ -0,0 +1,13 @@
+
+public class Question5 {
+
+	public static void main(String[] args) {
+		int[] no = {1, 8, 27, 64, 125};
+
+        for (int i = 0; i < no.length; i++) {
+        		System.out.println(no[i]);
+
+        }
+
+	}
+}
