d5cabdcf6dc33c0726b98b0d1bbe9d4861ddc510
nnchen <32369145+nnchen@users.noreply.github.com>
2017-11-05 14:12:20 -0800

    Add files via upload
    While Loops Solutions

---
+++ self coding/Question2.java
@@ -0,0 +1,15 @@
+
+public class Question2 {
+
+	public static void main(String[] args) {
+		int counter = 2;
+		
+		while (counter <= 10) {
+			System.out.println("" + counter);
+			counter += 2;
+		}
+		
+
+	}
+
+}
---
+++ self coding/Question3.java
@@ -0,0 +1,15 @@
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		int counter = 1;
+		
+		do{
+		
+		System.out.println(counter);
+		counter += 2;
+			
+		} while(counter <=10);
+		
+	}
+}
